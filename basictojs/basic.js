9f891d55cf499d2fbc1f2927426c372bce1dd037
ankit <ankitkalbhor3@gmail.com>
2025-12-11 17:12:23 +0530

    added to file at 11 dec17:05

---
+++ basictojs/basic.js
@@ -0,0 +1,11 @@
+// const accountId="0108it231008" 
+// let acccountEmail="ankitkalbhor"
+// var accountPassword="Ankit@1234"
+// var accountCity="Pune"
+// let name;
+// var age;
+
+// console.table({accountId,acccountEmail,accountPassword,accountCity,name,age})
+// console.log(typeof accountId)
+// console.log(typeof acccountEmail)
+// console.log(typeof 76886)
