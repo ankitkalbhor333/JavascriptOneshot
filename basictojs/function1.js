04fd912cef92ddba8cde121e14cbcbe0223cecd7
ankit <ankitkalbhor3@gmail.com>
2025-12-13 09:44:31 +0530

    adding file at 13 dec 2025

---
+++ basictojs/function1.js
@@ -0,0 +1,14 @@
+// let name="Ankit";
+// let fun1=function(name){
+//   console.log("hello "+name);
+
+// }
+// let store=fun1(name);
+ function newfun(object){
+console.log(`hello my name is ${object.usrname} ${object.surname} and my age is ${object.age}`);
+ }
+newfun({
+  usrname:"ankit",
+  age:22,
+  surname:"kalbhor"
+})
--- basictojs/object.js
+++ basictojs/object.js
@@ -1,10 +1,25 @@
-let Jsuser={
-  name:"ankit",
-  age:22,
-  city:"pune",
+// let Jsuser={
+//   name:"ankit",
+//   age:22,
+//   city:"pune",
   
+// }
+
+// const {name:n}=Jsuser;
+// console.log(n)
+// Jsuser.greeting=function(){
+//   console.log(`hello my name is ${this.name}`);
+// }
+// Jsuser.greeting();// 
+// Example: GET request
+async function getData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
-Jsuser.greeting=function(){
-  console.log(`hello my name is ${this.name}`);
-}
-Jsuser.greeting();// 
+
+getData();
