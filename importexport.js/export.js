9f309ab9db04c4628d5012ed85bc0839ce5c07f2
ankit <ankitkalbhor3@gmail.com>
2025-12-17 16:06:14 +0530

    17 dec

--- importexport.js/export.js
+++ importexport.js/export.js
@@ -1,8 +1,7 @@
-function sum(num1,num2){
-  return num1 + num2;
-}
-function multiply(num1,num2){
+ export const  sum=((num1,num2)=>{
+    return num1 + num2;
+ })
+export  const multiply=(num1,num2)=>{
    return num1 * num2;
 }
 
-module.exports = {sum,multiply};
--- importexport.js/import.js
+++ importexport.js/import.js
@@ -1,10 +1,10 @@
 // const impo=require('./export.js');
 // console.log(impo.sum(5,10));
 
-const impo=require('../outerfolder/index.js');
+// const impo=require('../outerfolder/index.js');
 
-
-
-console.log(impo[0]);
-console.log(impo[1]);
-console.log(impo);
+import {sum} from './export.js';
+console.log(sum(5,10));
+// console.log(impo[0]);
+// console.log(impo[1]);
+// console.log(impo);
