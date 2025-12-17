3ad2b478fa8e43c6905acb75c80c545fad4a7295
ankit <ankitkalbhor3@gmail.com>
2025-12-18 05:17:07 +0530

    Add user profile and random number features; implement data handling and update views

--- ejs/index.js
+++ ejs/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import data from './data.json' with { type: "json" };
+const instadata = data;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
+
+//serving static file 
+app.use(express.static("public"))
 
 // Tell Express where views are and set EJS as engine
 app.set("views", path.join(__dirname, "views"));
@@ -19,6 +24,25 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
   res.render("home", { title: "Home Page", message: "Welcome to EJS Home!" });
 });
+
+app.get('/root/:random', (req, res) => {
+  let arra=["apple","banana","grape","mango"];
+let randomNum = req.params.random;
+  res.render("randomnumber.ejs", { number: randomNum ,arr:arra});
+})
+
+app.get("/root1/:username",(req,res)=>{
+  let {username} = req.params;
+
+  // const instadat=require("./data.json");
+  const userData=instadata[username];
+
+  if(userData){
+    res.render("profile.ejs",{user:userData});
+  }else{
+    res.status(404).send("User not found");
+  }
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
